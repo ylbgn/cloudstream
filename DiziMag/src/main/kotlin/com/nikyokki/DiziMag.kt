a8a0bf619de2c5762ec7ffd53130b44c9747949a
root <root@localhost>
2025-01-22 14:44:03 +0300

    Dizimag

--- DiziMag/src/main/kotlin/com/nikyokki/DiziMag.kt
+++ DiziMag/src/main/kotlin/com/nikyokki/DiziMag.kt
@@ -1,9 +1,6 @@
-// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
-
 package com.nikyokki
 
 import CryptoJS
-import android.util.Log
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.lagradost.cloudstream3.Actor
 import com.lagradost.cloudstream3.Episode
@@ -11,6 +8,7 @@ import com.lagradost.cloudstream3.ErrorLoadingException
 import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.LoadResponse
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.MainPageRequest
 import com.lagradost.cloudstream3.SearchResponse
@@ -19,7 +17,6 @@ import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.fixUrlNull
 import com.lagradost.cloudstream3.mainPageOf
-import com.lagradost.cloudstream3.network.CloudflareKiller
 import com.lagradost.cloudstream3.newHomePageResponse
 import com.lagradost.cloudstream3.newMovieLoadResponse
 import com.lagradost.cloudstream3.newMovieSearchResponse
@@ -29,9 +26,6 @@ import com.lagradost.cloudstream3.toRatingInt
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.loadExtractor
-import com.lagradost.nicehttp.cookies
-import okhttp3.Interceptor
-import okhttp3.Response
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import java.util.regex.Pattern
@@ -45,10 +39,6 @@ class DiziMag : MainAPI() {
     override val hasChromecastSupport = true
     override val hasDownloadSupport = true
     override val supportedTypes = setOf(TvType.TvSeries, TvType.Movie)
-    var cf_clearance = ""
-    var ci_session = ""
-    var level = ""
-    var udys = ""
 
     // ! CloudFlare bypass
     override var sequentialMainPage =
@@ -58,29 +48,11 @@ class DiziMag : MainAPI() {
 
     override val mainPage = mainPageOf(
         "${mainUrl}/kesfet/eyJjYXRlZ29yeSI6W3siaWQiOiI4OSIsIm5hbWUiOiJBa3NpeW9uICYgTWFjZXJhIn1dfQ==" to "Aksiyon",
+        "${mainUrl}/kesfet/eyJjYXRlZ29yeSI6W3siaWQiOiI5MiIsIm5hbWUiOiJEcmFtIn1dfQ==" to "Dram",
+        "${mainUrl}/kesfet/eyJjYXRlZ29yeSI6W3siaWQiOiI4OCIsIm5hbWUiOiJCaWxpbSBLdXJndSAmIEZhbnRhemkifV19" to "Bilim Kurgu",
 
         "${mainUrl}/film/tur/aile" to "Aile Film"
     )
-
-    private val cloudflareKiller by lazy { CloudflareKiller() }
-    private val interceptor by lazy { CloudflareInterceptor(cloudflareKiller) }
-
-    class CloudflareInterceptor(private val cloudflareKiller: CloudflareKiller) : Interceptor {
-        override fun intercept(chain: Interceptor.Chain): Response {
-            println("Chain: $chain")
-            val request = chain.request()
-            println("Request: $request")
-            val response = chain.proceed(request)
-            println("Request: $response")
-            val doc = Jsoup.parse(response.peekBody(1024 * 1024).string())
-            val intercept = cloudflareKiller.intercept(chain)
-            println("Saved Cookies: ${cloudflareKiller.savedCookies}")
-            println("Cookies: ${intercept.cookies}")
-            return intercept
-
-            //return response
-        }
-    }
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val mainReq = app.get("${request.data}/${page}")
@@ -111,74 +83,49 @@ class DiziMag : MainAPI() {
         }
     }
 
-    private fun toSearchResponse(ad: String, link: String, posterLink: String): SearchResponse? {
-        if (link.contains("dizi")) {
-            return newTvSeriesSearchResponse(
-                ad ?: return null,
-                link,
-                TvType.TvSeries,
-            ) {
-                this.posterUrl = posterLink
-            }
-        } else {
-            return newMovieSearchResponse(
-                ad ?: return null,
-                link,
-                TvType.Movie,
-            ) {
-                this.posterUrl = posterLink
-            }
+    private fun Element.toPostSearchResult(): SearchResponse? {
+        val title = this.selectFirst("span")?.text()?.trim() ?: return null
+        val href = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("data-src"))
+
+        return newTvSeriesSearchResponse(title, href, TvType.TvSeries) {
+            this.posterUrl = posterUrl
         }
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val mainReq = app.get(mainUrl)
-        val mainPage = mainReq.document
-        val cKey = mainPage.selectFirst("input[name='cKey']")?.attr("value") ?: return emptyList()
-        val cValue =
-            mainPage.selectFirst("input[name='cValue']")?.attr("value") ?: return emptyList()
-        val cookie = mainReq.cookies["PHPSESSID"].toString()
-        println("Ckey: $cKey ---- Cvalue: $cValue ---- cookie: $cookie")
-
         val veriler = mutableListOf<SearchResponse>()
 
         val searchReq = app.post(
-            "${mainUrl}/bg/searchcontent",
+            "${mainUrl}/search",
             data = mapOf(
-                "cKey" to cKey,
-                "cValue" to cValue,
-                "searchterm" to query
+                "query" to query
             ),
             headers = mapOf(
+                "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
                 "Accept" to "application/json, text/javascript, */*; q=0.01",
                 "X-Requested-With" to "XMLHttpRequest",
-                "Cookie" to "PHPSESSID=$cookie"
+                "Content-Type" to "application/x-www-form-urlencoded; charset=UTF-8",
+                "Accept-Language" to "en-US,en;q=0.5"
             ),
-            referer = "${mainUrl}/",
-            cookies = mapOf(
-                "CNT" to "vakTR",
-                "PHPSESSID" to cookie
-            )
+            referer = "${mainUrl}/"
         ).parsedSafe<SearchResult>()
-        println("SearchReq: $searchReq")
 
         if (searchReq?.success != true) {
             throw ErrorLoadingException("Invalid Json response")
         }
 
-        val searchDoc = searchReq.theme?.trim()
+        val searchDoc = searchReq.theme
 
-        searchDoc?.trim()?.split("</a>")?.forEach { item ->
+        val document = Jsoup.parse(searchDoc.toString())
+        val results = mutableListOf<SearchResponse>()
 
-            val bb = item.substringAfter("<a href=\"").substringBefore("\"")
-            val diziUrl = bb.trim()
-            val cc = item.substringAfter("data-srcset=\"").substringBefore(" 1x")
-            val posterLink = cc.trim()
-            val dd = item.substringAfter("<span class=\"text-white\">").substringBefore("</span>")
-            val ad = dd.trim()
-            toSearchResponse(ad, diziUrl, posterLink)?.let { veriler.add(it) }
-
-
+        document.select("ul li").forEach { listItem ->
+            val href = listItem.selectFirst("a")?.attr("href")
+            if (href != null && (href.contains("/dizi/") || href.contains("/film/"))) {
+                val result = listItem.toPostSearchResult()
+                result?.let { results.add(it) }
+            }
         }
         return veriler
     }
@@ -189,48 +136,37 @@ class DiziMag : MainAPI() {
 
         val mainReq = app.get(url, referer = mainUrl)
         val document = mainReq.document
-
+        val title =
+            document.selectFirst("div.page-title h1")?.selectFirst("a")?.text() ?: return null
+        val poster =
+            fixUrlNull(document.selectFirst("div.series-profile-image img")?.attr("src"))
+        val year =
+            document.selectFirst("h1 span")?.text()?.substringAfter("(")?.substringBefore(")")
+                ?.toIntOrNull()
+        val rating = document.selectFirst("span.color-imdb")?.text()?.trim()?.toRatingInt()
+        val duration =
+            document.selectXpath("//span[text()='Süre']//following-sibling::p").text().trim()
+                .split(" ").first().toIntOrNull()
+        val description = document.selectFirst("div.series-profile-summary p")?.text()?.trim()
+        val tags = document.selectFirst("div.series-profile-type")?.select("a")
+            ?.mapNotNull { it.text().trim() }
+        val trailer = document.selectFirst("div.series-profile-trailer")?.attr("data-yt")
+        val actors = mutableListOf<Actor>()
+        document.select("div.series-profile-cast li").forEach {
+            val img = fixUrlNull(it.selectFirst("img")?.attr("data-src"))
+            val name = it.selectFirst("h5.truncate")?.text()?.trim() ?: return null
+            actors.add(Actor(name, img))
+        }
         if (url.contains("/dizi/")) {
-            val title =
-                document.selectFirst("div.page-title h1")?.selectFirst("a")?.text() ?: return null
-            val poster =
-                fixUrlNull(document.selectFirst("div.series-profile-image img")?.attr("src"))
-            var year = document.select("li.w-auto.sm").last()?.select("p")?.text()?.toIntOrNull()
-            val description = document.selectFirst("div.series-profile-summary p")?.text()?.trim()
-            val tags = document.selectFirst("div.series-profile-type")?.select("a")
-                ?.mapNotNull { it.text().trim() }
-            var rating = 0
-
-            document.select("filter-result-box-bottom li").forEach { it ->
-                if (it.selectFirst("span")?.text()?.contains("Puan") == true) {
-                    rating = it.select("span.color-imdb").text().toRatingInt()!!
-                }
-                if (it.selectFirst("span")?.text()?.contains("Yapım yılı") == true) {
-                    year = it.selectFirst("p")?.text()?.toIntOrNull()
-                }
-            }
-            val actors = mutableListOf<Actor>()
-            document.select("div.series-profile-cast li").forEach { it ->
-                val img = fixUrlNull(it.selectFirst("img")?.attr("data-src"))
-                val name = it.selectFirst("h5.truncate")?.text()?.trim() ?: return null
-                actors.add(Actor(name, img))
-            }
-
             val episodeses = mutableListOf<Episode>()
             var szn = 1
             for (sezon in document.select("div.series-profile-episode-list")) {
                 var blm = 1
                 for (bolum in sezon.select("li")) {
                     val epName = bolum.selectFirst("h6.truncate a")?.text() ?: continue
-                    Log.d("DMG", "epName: $epName")
                     val epHref = fixUrlNull(bolum.select("h6.truncate a").attr("href")) ?: continue
-                    Log.d("DMG", "epHref: $epHref")
                     val epEpisode = blm++
-                    Log.d("DMG", "epEpisode: $epEpisode")
-                    //val epSeason  = bolum.selectFirst("div.seasons-menu")?.text()?.substringBefore(".Sezon")?.trim()?.toIntOrNull()
                     val epSeason = szn
-                    Log.d("DMG", "epSeason: $epSeason")
-
                     episodeses.add(
                         Episode(
                             data = epHref,
@@ -251,42 +187,20 @@ class DiziMag : MainAPI() {
                 this.tags = tags
                 this.rating = rating
                 addActors(actors)
+                addTrailer("https://www.youtube.com/embed/${trailer}")
             }
         } else {
-            var yil: Int? = null;
-            val title = document.selectFirst("div.poster.hidden h2")?.text() ?: return null
-            val poster = fixUrlNull(
-                document.selectFirst("div.flex.items-start.gap-4.slider-top img")?.attr("src")
-            )
-            val description = document.selectFirst("div.mt-2.text-md")?.text()?.trim()
-            document.select("div.flex.items-center").forEach { item ->
-                if (item.selectFirst("span")?.text()?.contains("tarih") == true) {
-                    yil = item.selectFirst("div.w-fit.rounded-lg")?.text()?.toIntOrNull()
-                }
-            }
-            val tags =
-                document.select("div.text-white.text-md.opacity-90.flex.items-center.gap-2.overflow-auto.mt-1 a")
-                    .map { it.text() }
-            val rating =
-                document.selectFirst("div.flex.items-center")
-                    ?.selectFirst("span.text-white.text-sm")
-                    ?.text()?.trim().toRatingInt()
-            val actors = mutableListOf<Actor>()
-            document.select("div.w-fit.min-w-fit.rounded-lg").forEach { a ->
-                if (a.selectFirst("span")?.text()?.contains("Aktör") == true) {
-                    actors.add(Actor(a.selectFirst("a")?.text()!!))
-                }
-            }
             return newMovieLoadResponse(title, url, TvType.Movie, url) {
                 this.posterUrl = poster
-                this.year = yil
+                this.year = year
                 this.plot = description
-                this.rating = rating
                 this.tags = tags
+                this.rating = rating
+                this.duration = duration
                 addActors(actors)
+                addTrailer("https://www.youtube.com/embed/${trailer}")
             }
         }
-
     }
 
     override suspend fun loadLinks(
@@ -295,35 +209,28 @@ class DiziMag : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        Log.d("DMG", "data » ${data}")
         val headers = mapOf(
             "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
             "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Referer" to "$mainUrl/"
         )
-        var aa = app.get(mainUrl)
-        val ci_session = aa.cookies["ci_session"].toString()
+        val aa = app.get(mainUrl)
+        val ciSession = aa.cookies["ci_session"].toString()
         val document = app.get(
             data, headers = headers, cookies = mapOf(
-                "ci_session" to ci_session
+                "ci_session" to ciSession
             )
         ).document
-        var iframe =
+        val iframe =
             fixUrlNull(document.selectFirst("div#tv-spoox2 iframe")?.attr("src")) ?: return false
-        Log.d("DMG", "iframe » ${iframe}")
-
-
         val docum = app.get(iframe, headers = headers, referer = "$mainUrl/").document
         docum.select("script").forEach { sc ->
             if (sc.toString().contains("bePlayer")) {
-                Log.d("DMG", "bePlayer var")
                 val pattern = Pattern.compile("bePlayer\\('(.*?)', '(.*?)'\\)")
                 val matcher = pattern.matcher(sc.toString().trimIndent())
                 if (matcher.find()) {
                     val key = matcher.group(1)
                     val jsonCipher = matcher.group(2)
-                    Log.d("DMG", "key » $key")
-                    Log.d("DMG", "jsonCipher » $jsonCipher")
                     val cipherData = ObjectMapper().readValue(
                         jsonCipher?.replace("\\/", "/"),
                         Cipher::class.java
@@ -331,20 +238,18 @@ class DiziMag : MainAPI() {
                     val ctt = cipherData.ct
                     val iv = cipherData.iv
                     val s = cipherData.s
-                    Log.d("DMG", "ctt » $ctt")
-                    Log.d("DMG", "iv » $iv")
-                    Log.d("DMG", "ctt » $s")
                     val decrypt = key?.let { CryptoJS.decrypt(it, ctt, iv, s) }
-                    Log.d("DMG", "decrypt » $decrypt")
 
                     val jsonData = ObjectMapper().readValue(decrypt, JsonData::class.java)
-                    Log.d("DMG", "jsonData » $jsonData")
 
-                    val m3u8Content = app.get(jsonData.videoLocation, referer = iframe, headers = mapOf("Accept" to "*/*", "Referer" to iframe)).document.body()
+                    val m3u8Content = app.get(
+                        jsonData.videoLocation,
+                        referer = iframe,
+                        headers = mapOf("Accept" to "*/*", "Referer" to iframe)
+                    ).document.body()
                     val regex = Regex("#EXT-X-STREAM-INF:.*? (https?://\\S+)")
                     val matchResult = regex.find(m3u8Content.text())
                     val m3uUrl = matchResult?.groupValues?.get(1) ?: ""
-                    Log.d("DMG", m3uUrl)
                     callback.invoke(
                         ExtractorLink(
                             source = this.name,
