21ec3f19a21310debd61fa7ad996456c967c1996
root <root@localhost>
2025-01-23 09:56:54 +0300

    YabanciDizi

--- YabanciDizi/src/main/kotlin/com/nikyokki/YabanciDizi.kt
+++ YabanciDizi/src/main/kotlin/com/nikyokki/YabanciDizi.kt
@@ -54,7 +54,7 @@ class YabanciDizi : MainAPI() {
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("${request.data}${page}").document
+        val document = app.get("${request.data}/${page}").document
         val home     = document.select("div.mofy-movbox").mapNotNull { it.toSearchResult() }
 
         return newHomePageResponse(request.name, home)
