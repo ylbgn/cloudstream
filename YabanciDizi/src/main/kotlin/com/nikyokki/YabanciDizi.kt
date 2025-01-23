6de306f60c3c254375ae7cced7a6942a4850cdf3
root <root@localhost>
2025-01-23 11:05:20 +0300

    YabanciDizi

--- YabanciDizi/src/main/kotlin/com/nikyokki/YabanciDizi.kt
+++ YabanciDizi/src/main/kotlin/com/nikyokki/YabanciDizi.kt
@@ -106,7 +106,6 @@ class YabanciDizi : MainAPI() {
         val headers = mapOf("Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
         val document = app.get(url, referer = mainUrl, headers = headers).document
-        Log.d("YBD", document.toString())
         val title       = document.selectFirst("h1 a")?.text()?.trim() ?: return null
         Log.d("YBD",title)
         val poster      = fixUrlNull(document.selectFirst("div#series-profile-wrapper img")?.attr("src")) ?: return null
