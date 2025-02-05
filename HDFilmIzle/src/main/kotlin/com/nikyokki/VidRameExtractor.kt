93600ac2c54712ce950b1eebe9302a819cff57b9
root <root@localhost>
2025-02-05 17:22:59 +0300

    HDFilmIzle

--- HDFilmIzle/src/main/kotlin/com/nikyokki/VidRameExtractor.kt
+++ HDFilmIzle/src/main/kotlin/com/nikyokki/VidRameExtractor.kt
@@ -59,7 +59,7 @@ open class VidRameExtractor : ExtractorApi() {
             headers = mapOf("User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0",
                 "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                 "Accept-Language" to "en-US,en;q=0.5"),
-            referer = mainUrl
+            referer = referer
         ).document.select("script").find { it.data().contains("sources:") }?.data() ?: ""
 
         val regex = """("file": )EE\.dd\(".*?"\)""".toRegex()
