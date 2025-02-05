22001087407e6b26a9ef51a86a8e16ced724ab71
root <root@localhost>
2025-02-05 17:20:26 +0300

    HDFilmIzle

--- HDFilmIzle/src/main/kotlin/com/nikyokki/VidRameExtractor.kt
+++ HDFilmIzle/src/main/kotlin/com/nikyokki/VidRameExtractor.kt
@@ -56,6 +56,9 @@ open class VidRameExtractor : ExtractorApi() {
         Log.d("VidEx", url)
         val script = app.get(
             url,
+            headers = mapOf("User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0",
+                "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Language" to "en-US,en;q=0.5"),
             referer = mainUrl
         ).document.select("script").find { it.data().contains("sources:") }?.data() ?: ""
 
