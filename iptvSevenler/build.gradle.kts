c99a2317f4b84cdb67627671d71f6c141a5f1b75
patr0n <eren1371@gmail.com>
2025-03-19 23:46:39 +0300

    iptvsevenler, vavooSpor, powerSinema eklemesi yapıldı

---
+++ iptvSevenler/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 2
+
+cloudstream {
+    authors     = listOf("patr0n")
+    language    = "tr"
+    description = "iptvSevenler"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=tr.canlitv.team&sz=%size%"
+}
