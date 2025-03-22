6052f39d1dd8d409e8b0269e8719162de002e3c5
patr0n <105314623+patr0nq@users.noreply.github.com>
2025-03-22 22:51:59 +0300

    Update Vavoo.kt

--- Vavoo/src/main/kotlin/com/nikyokki/Vavoo.kt
+++ Vavoo/src/main/kotlin/com/nikyokki/Vavoo.kt
@@ -40,7 +40,7 @@ class Vavoo() : MainAPI() {
             "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "Accept-Language" to "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
             "Referer" to "https://vavoo.to/",
-            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+            "User-Agent" to "VAVOO/1.0"
         )
 
         const val posterLink = "https://raw.githubusercontent.com/doGior/doGiorsHadEnough/master/Huhu/tv.png"
