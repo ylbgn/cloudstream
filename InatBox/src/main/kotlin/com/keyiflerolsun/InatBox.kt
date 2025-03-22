5c0da928fb697e1543eb567c35063b0d5798c142
patr0n <105314623+patr0nq@users.noreply.github.com>
2025-03-22 23:45:35 +0300

    Update InatBox.kt

--- InatBox/src/main/kotlin/com/keyiflerolsun/InatBox.kt
+++ InatBox/src/main/kotlin/com/keyiflerolsun/InatBox.kt
@@ -16,7 +16,7 @@ import org.json.JSONException
 import org.json.JSONObject
 
 class InatBox : MainAPI() {
-    private val contentUrl  = "https://dizibox.rest"
+    private val contentUrl  = "https://dizibox.live"
 
     override var name                 = "InatBox"
     override val hasMainPage          = true
