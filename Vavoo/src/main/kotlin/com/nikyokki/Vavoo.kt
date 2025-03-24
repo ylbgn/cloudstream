3f3123f6a993c3a5a25ade914fa36bab0c11f30c
patr0n <eren1371@gmail.com>
2025-03-25 00:33:37 +0300

    Update Vavoo.kt

--- Vavoo/src/main/kotlin/com/nikyokki/Vavoo.kt
+++ Vavoo/src/main/kotlin/com/nikyokki/Vavoo.kt
@@ -2,19 +2,7 @@ package com.nikyokki
 
 import android.util.Log
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.lagradost.cloudstream3.HomePageList
-import com.lagradost.cloudstream3.HomePageResponse
-import com.lagradost.cloudstream3.LiveSearchResponse
-import com.lagradost.cloudstream3.LoadResponse
-import com.lagradost.cloudstream3.TvType
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.MainPageRequest
-import com.lagradost.cloudstream3.SearchResponse
-import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.VPNStatus
-import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.newHomePageResponse
-import com.lagradost.cloudstream3.newLiveStreamLoadResponse
+import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
@@ -30,7 +18,7 @@ class Vavoo() : MainAPI() {
     override val vpnStatus = VPNStatus.MightBeNeeded
 
     private suspend fun getChannels(): List<Channel> {
-        val document = app.get("$mainUrl/channels").body.string()
+        val document = app.get("$mainUrl/channels", headers = headers).body.string()
         return parseJson<List<Channel>>(document)
     }
 
@@ -39,7 +27,8 @@ class Vavoo() : MainAPI() {
         val headers = mapOf(
             "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "Accept-Language" to "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-            "Referer" to "https://vavoo.to/",
+            "Connection" to "keep-alive",
+            "Referer" to "https://vavoo.to",
             "User-Agent" to "VAVOO/1.0"
         )
 
@@ -51,11 +40,7 @@ class Vavoo() : MainAPI() {
             channels = getChannels()
         }
         val sections = channels.groupBy { it.country }.map {
-            HomePageList(
-                it.key,
-                it.value.map { channel -> channel.toSearchResponse(this.name) },
-                false
-            )
+            HomePageList(it.key, it.value.map { channel -> channel.toSearchResponse(this.name) }, false)
         }.sortedBy { it.name }
 
         return newHomePageResponse(sections, false)
@@ -66,20 +51,15 @@ class Vavoo() : MainAPI() {
             channels = getChannels()
         }
         val matches = channels.filter { channel ->
-            query.lowercase().replace(" ", "") in
-                    channel.name.lowercase().replace(" ", "")
+            query.lowercase().replace(" ", "") in channel.name.lowercase().replace(" ", "")
         }
         return matches.map { it.toSearchResponse(this.name) }
     }
 
     override suspend fun load(url: String): LoadResponse {
-        Log.d("Vavoo", "Loading channel: $url")
+        Log.d("TV2", url)
         val channel = parseJson<Channel>(url)
-        return newLiveStreamLoadResponse(
-            channel.name, 
-            "$mainUrl/play/${channel.id}/index.m3u8", 
-            "$mainUrl/play/${channel.id}/index.m3u8"
-        ) {
+        return newLiveStreamLoadResponse(channel.name, "$mainUrl/play/${channel.id}/index.m3u8", "$mainUrl/play/${channel.id}/index.m3u8") {
             this.posterUrl = posterLink
             this.tags = listOf(channel.country)
         }
@@ -91,14 +71,14 @@ class Vavoo() : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit,
     ): Boolean {
-        Log.d("Vavoo", "Loading links: $data")
+        Log.d("TV2", data)
         callback(
             ExtractorLink(
                 this.name,
                 this.name,
                 data,
                 headers = headers,
-                referer = "$mainUrl/",
+                referer = mainUrl,
                 quality = Qualities.Unknown.value,
                 isM3u8 = true
             )
@@ -107,10 +87,14 @@ class Vavoo() : MainAPI() {
     }
 
     data class Channel(
-        @JsonProperty("country") val country: String,
-        @JsonProperty("id") val id: Long,
-        @JsonProperty("name") val name: String,
-        @JsonProperty("p") val p: Int
+        @JsonProperty("country")
+        val country: String,
+        @JsonProperty("id")
+        val id: Long,
+        @JsonProperty("name")
+        val name: String,
+        @JsonProperty("p")
+        val p: Int
     ) {
         fun toSearchResponse(apiName: String): LiveSearchResponse {
             return LiveSearchResponse(
