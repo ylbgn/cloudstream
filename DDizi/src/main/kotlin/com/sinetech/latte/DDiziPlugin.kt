6e66fc00fd9966fda94a8068fa7f304f66fa9959
patr0n <105314623+patr0nq@users.noreply.github.com>
2025-04-13 13:08:18 +0300

    Add files via upload

---
+++ DDizi/src/main/kotlin/com/sinetech/latte/DDiziPlugin.kt
@@ -0,0 +1,12 @@
+package com.sinetech.latte
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class DDiziPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(DDiziProvider())
+    }
+} 
