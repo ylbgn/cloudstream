9ae0e34bd6714d0977b6ca05bc7d6524ad1f36ff
patr0n <eren1371@gmail.com>
2025-05-15 17:45:43 +0300

    selcuksport_sakultahEklemesi

---
+++ Selcuksports/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 5
+
+cloudstream {
+    authors     = listOf("Sakultah")
+    language    = "tr"
+    description = "TRGoals"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=tr.canlitv.team&sz=%size%"
+}
