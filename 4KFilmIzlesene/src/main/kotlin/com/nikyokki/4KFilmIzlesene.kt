10f888becb07c28ba3bb179da2a7680d0f048b64
patr0n <105314623+patr0nq@users.noreply.github.com>
2025-05-17 01:13:49 +0300

    Update 4KFilmIzlesene.kt

--- 4KFilmIzlesene/src/main/kotlin/com/nikyokki/4KFilmIzlesene.kt
+++ 4KFilmIzlesene/src/main/kotlin/com/nikyokki/4KFilmIzlesene.kt
@@ -33,6 +33,11 @@ class `4KFilmIzlesene` : MainAPI() {
     override val hasDownloadSupport = true
     override val supportedTypes = setOf(TvType.Movie)
 
+    // ! CloudFlare bypass
+    override var sequentialMainPage = true        // * https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-main-a-p-i/index.html#-2049735995%2FProperties%2F101969414
+    override var sequentialMainPageDelay       = 50L  // ? 0.05 saniye
+    override var sequentialMainPageScrollDelay = 50L  // ? 0.05 saniye
+
     override val mainPage = mainPageOf(
         "${mainUrl}/"                               to "Yeni Filmler",
         "${mainUrl}/"                               to "Trend Filmler",
