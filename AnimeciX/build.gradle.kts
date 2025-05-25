7c6158e0c283c7960888bbd68f7f58de0a2e7182
Mehmet UYSAL <74021632+ylbgn@users.noreply.github.com>
2025-05-25 21:29:11 +0300

    Update build.gradle.kts

--- AnimeciX/build.gradle.kts
+++ AnimeciX/build.gradle.kts
@@ -1,7 +1,7 @@
 version = 9
 
 cloudstream {
-    authors     = listOf("keyiflerolsun", "inatchii", "JustRelaxable", "nikyokki")
+    authors     = listOf("keyiflerolsun", "inatchii", "ylbgn", "JustRelaxable", "nikyokki")
     language    = "tr"
     description = "AnimeciX, animeleri keşfedebileceğiniz ve animeler hakkında her şeyi öğrenebileceğiniz bir paylaşım platformudur."
 
